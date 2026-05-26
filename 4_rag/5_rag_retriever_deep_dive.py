908ff65ca7b6839e47c280c71f5efd13c64c718f
Jobi S S <jobisjobi1234@gmail.com>
2026-05-26 22:37:18 +0530

    retrieval model

--- 4_rag/5_rag_retriever_deep_dive.py
+++ 4_rag/5_rag_retriever_deep_dive.py
@@ -1,8 +1,9 @@
 import os
 
 from dotenv import load_dotenv
-from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+# from langchain_openai import OpenAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 load_dotenv()
 
@@ -12,7 +13,8 @@ db_dir = os.path.join(current_dir, "db")
 persistent_directory = os.path.join(db_dir, "chroma_db_with_metadata")
 
 # Define the embedding model
-embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+# embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")  # Update to a valid embedding model if needed
 
 # Load the existing vector store with the embedding function
 db = Chroma(persist_directory=persistent_directory,
